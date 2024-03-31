a3a2d2c3eb322bf5644e31ec0fc3dabb27e2d280
Eric Alan Rodriguez Contreras <licalan.sistemas@gmail.com>
2024-03-30 20:02:23 -0600

    FlutterBloc and Cubits Implementation

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,22 @@
+import 'package:forms_app/presentation/screens/screens.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen()
+    ),
+
+    //*Cubits
+    GoRoute(
+      path: '/cubits',
+      builder: (context, state) => const CubitCounterScreen()
+    ),
+    //*Flutter Bloc
+    GoRoute(
+      path: '/counter-bloc',
+      builder: (context, state) => const BlocCounterScreen()
+    )
+  ]
+);
