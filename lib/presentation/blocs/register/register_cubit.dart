3b712f90ef16ff35ab3d79d028feaf2abd98829b
Eric Alan Rodriguez Contreras <licalan.sistemas@gmail.com>
2024-03-31 20:16:30 -0600

    Form and StateManagement

---
+++ lib/presentation/blocs/register/register_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'register_state.dart';
+
+class RegisterCubit extends Cubit<RegisterFormState> {
+  RegisterCubit() : super(const RegisterFormState());
+
+  void onSubmit() {
+    print('Cubit Submit $state');
+    emit(state.copyWith(formStatus: FormStatus.posting));
+  }
+
+  void usernameChanged(String value) {
+    emit(state.copyWith(username: value));
+  }
+
+  void emailChanged(String value) {
+    emit(state.copyWith(email: value));
+  }
+
+  void passwordChanged(String value) {
+    emit(state.copyWith(password: value));
+  }
+}
